c62bafe4e7238e06b94cf73f19a96ef804f1d7fe
Alexandre <alex.chevreaud@icloud.com>
2021-12-24 18:38:12 +0100

    Jpa fonctionnelle pour Entreprise avec param

--- InfoRepartieV2/src/main/java/Y2A/InfoRepartieV2/Controller/EntrepriseController.java
+++ InfoRepartieV2/src/main/java/Y2A/InfoRepartieV2/Controller/EntrepriseController.java
@@ -1,7 +1,7 @@
 package Y2A.InfoRepartieV2.Controller;
 
-
 import Y2A.InfoRepartieV2.Service.EntrepriseService;
+import Y2A.InfoRepartieV2.models.Entreprise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +13,8 @@ public class EntrepriseController {
     @Autowired
     private EntrepriseService entrepriseService;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return entrepriseService.findById(1).get().toString();
+    @GetMapping("/Entreprise")
+    public Entreprise hello(@RequestParam int id) {
+        return entrepriseService.findById(id).get();
     }
 }
--- InfoRepartieV2/src/main/java/Y2A/InfoRepartieV2/Repository/EntrepriseRepository.java
+++ InfoRepartieV2/src/main/java/Y2A/InfoRepartieV2/Repository/EntrepriseRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface EntrepriseRepository extends CrudRepository <Entreprise,Integer> {
-
-
 }
