121addc98c2181abdfacf55bebe3f435ca1cf5c4
Alexandre <alex.chevreaud@icloud.com>
2022-01-14 10:23:25 +0100

    Tables pour les forms

--- InfoRepartieV2/src/main/java/Y2A/InfoRepartieV2/Repository/EntrepriseRepository.java
+++ InfoRepartieV2/src/main/java/Y2A/InfoRepartieV2/Repository/EntrepriseRepository.java
@@ -1,10 +1,11 @@
 package Y2A.InfoRepartieV2.Repository;
 
 import Y2A.InfoRepartieV2.models.Entreprise;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EntrepriseRepository extends CrudRepository<Entreprise, Integer> {
+public interface EntrepriseRepository extends JpaRepository<Entreprise, Integer> {
+
 
 }
